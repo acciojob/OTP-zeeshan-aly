e400d8ba74ae2adea24840bdeb12eb661fb563a9
Syed Mohd Zeeshan Ali <68470786+zeeshan-aly@users.noreply.github.com>
2024-01-25 16:32:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function focusNext(currentInput) {
+    const nextInput = currentInput.nextElementSibling;
+    const previousInput = currentInput.previousElementSibling;
+
+    if (event.inputType === 'deleteContentBackward' && previousInput) {
+        previousInput.focus();
+    } else if (nextInput) {
+        nextInput.focus();
+    }
+}
